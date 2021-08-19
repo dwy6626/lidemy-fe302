e01bc176168237b4ce71ecf3ebe7019bb8b91d80
David Ye <david@dwye.dev>
2021-08-19 17:18:22 +0800

    functional counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+
+const TitleStyle = {
+  color: 'blue',
+  textAlign: 'center'
+}
+
+function Title() {
+  return <h1 style={TitleStyle}>Hello</h1>
+}
+
+function updateCountToStorage(count) {
+  window.localStorage.setItem('count', JSON.stringify(count))
+}
+
+function Counter() {
+  const localCount = window.localStorage.getItem('count')
+  const initCount = localCount ? JSON.parse(localCount) : 0
+  const [count, setCount] = React.useState(initCount)
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+  React.useEffect(() => {
+    document.title = `You clicked ${count} times`;
+    updateCountToStorage(count)
+  })
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={handleClick}>click me</button>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+      Hello World!
+      <Counter/>
     </div>
   );
 }
